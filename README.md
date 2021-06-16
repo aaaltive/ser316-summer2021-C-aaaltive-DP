461783c9c74350558f5cd0eeedf106eb07df1412
Armand Altiveros <aaaltive@gmail.com>
2021-06-16 12:02:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ser316-SUM2021-C-aaaltive-DP-
+Assignment 5a/b - Design Patterns
---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+/**
+ * This is the Main class, the starting point for running the program.
+ *
+ * @author Armand Altiveros
+ * @version 1.0
+ */
+
+public class Main {
+
+    /**
+     * The starting point for the program.
+     * @param args arguments, probably none.
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Waddup, world.");
+    }
+}
